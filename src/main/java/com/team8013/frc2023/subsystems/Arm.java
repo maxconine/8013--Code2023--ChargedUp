a65a1e3701c17d409c914bc16a320282ec521def
leo8myairpod <58615713+leo8myairpod@users.noreply.github.com>
2023-02-08 17:31:42 -0500

    Update Arm.java

--- src/main/java/com/team8013/frc2023/subsystems/Arm.java
+++ src/main/java/com/team8013/frc2023/subsystems/Arm.java
@@ -1,22 +1,22 @@
-// package com.team8013.frc2023.subsystems;
+package com.team8013.frc2023.subsystems;
 
-// import java.util.ArrayList;
+import java.util.ArrayList;
 
-// import com.ctre.phoenix.motorcontrol.ControlMode;
-// import com.ctre.phoenix.motorcontrol.NeutralMode;
-// import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
-// import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
-// import com.ctre.phoenix.motorcontrol.can.TalonFX;
-// import com.team8013.frc2023.Constants;
-// import com.team8013.frc2023.Ports;
-// import com.team8013.frc2023.logger.LogStorage;
-// import com.team8013.frc2023.logger.LoggingSystem;
-// import com.team8013.frc2023.subsystems.ServoMotorSubsystem.ControlState;
-// import com.team254.lib.drivers.TalonFXFactory;
-// import com.team254.lib.util.Util;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.team8013.frc2023.Constants;
+import com.team8013.frc2023.Ports;
+import com.team8013.frc2023.logger.LogStorage;
+import com.team8013.frc2023.logger.LoggingSystem;
+import com.team8013.frc2023.subsystems.ServoMotorSubsystem.ControlState;
+import com.team254.lib.drivers.TalonFXFactory;
+import com.team254.lib.util.Util;
 
-// import edu.wpi.first.wpilibj.Timer;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 package frc.robot.Subsystems;
 
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
@@ -25,6 +25,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 public class Arm extends Subsystem {
     
     private static Arm mInstance;
+    
     public static synchronized Arm getInstance() {
         if (mInstance == null) {
             mInstance = new Arm();
