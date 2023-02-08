56bbbc3f25dd14493dbfa74a98f6d1401bec222d
leo8myairpod <58615713+leo8myairpod@users.noreply.github.com>
2023-02-08 17:28:41 -0500

    Update Arm.java

--- src/main/java/com/team8013/frc2023/subsystems/Arm.java
+++ src/main/java/com/team8013/frc2023/subsystems/Arm.java
@@ -17,301 +17,58 @@
 
 // import edu.wpi.first.wpilibj.Timer;
 // import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+package frc.robot.Subsystems;
 
-// public class Arm extends Subsystem {
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
-//     private static Arm mInstance;
-//     public static synchronized Arm getInstance() {
-//         if (mInstance == null) {
-//             mInstance = new Arm();
-//         } 
-//         return mInstance;
-//     }
-
-//     private final TalonFX mArm;
-
-//     public boolean mHomed;
-
-//     // logger
-//     LogStorage<PeriodicIO> mStorage = null;
+public class Arm extends Subsystem {
     
-//     // status variable for is enabled
-//     public boolean mIsEnabled = false;
-
-//     public ArmControlState mArmControlState = ArmControlState.OPEN_LOOP;
-
-//     private boolean mExtendArm = false;
-//     private boolean mPartialExtendArm = false;
-
-//     public PeriodicIO mPeriodicIO = new PeriodicIO();
-
-//     public StatorCurrentLimitConfiguration STATOR_CURRENT_LIMIT = new StatorCurrentLimitConfiguration(true, 60, 60, .2);
-
-//     private Arm() {
-//         mArm = TalonFXFactory.createDefaultTalon(Ports.ARM_ID);
-
-//         // for right motor
-//         mArm.set(ControlMode.PercentOutput, 0);
-//         mArm.setInverted(false);
-//         mArm.setSelectedSensorPosition(0.0);
-
-//         mArm.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, Constants.kLongCANTimeoutMs);
-
-//         mArm.configMotionAcceleration(40000, Constants.kLongCANTimeoutMs);
-//         mArm.configMotionCruiseVelocity(20000, Constants.kLongCANTimeoutMs);
-//         mArm.config_kP(0, 0.6);
-//         mArm.config_kI(0, 0);
-//         mArm.config_kD(0, 0);
-//         mArm.config_kF(0, 0.077);
-
-//         mArm.setNeutralMode(NeutralMode.Brake);
-
-//         mArm.configVoltageCompSaturation(12.0, Constants.kLongCANTimeoutMs);
-//         mArm.enableVoltageCompensation(true);
-
-
-//         // set current limits on motor
-//         mArm.configStatorCurrentLimit(STATOR_CURRENT_LIMIT);
-//     }
-
-//     @Override
-//     public void readPeriodicInputs() {
-        
-//         // curr check for holding
-//         maybeHoldCurrentPosition();
-
-//         mPeriodicIO.arm_voltage = mArm.getMotorOutputVoltage();
-//         mPeriodicIO.arm_stator_current = mArm.getStatorCurrent();
-//         mPeriodicIO.arm_motor_velocity = mArm.getSelectedSensorVelocity();
-//         mPeriodicIO.arm_motor_position = mArm.getSelectedSensorPosition();
-
-//         // send log data
-//         SendLog();
-//     }
-
-//     @Override
-//     public void writePeriodicOutputs() {
-//         // set status variable for being enabled to true
-//         mIsEnabled = true;
-
-//         switch (mArmControlState) {
-//             case OPEN_LOOP:
-//                 mArm.set(ControlMode.PercentOutput, mPeriodicIO.arm_demand / 12.0);
-//                 break;
-//             case MOTION_MAGIC:
-//                 mArm.set(ControlMode.MotionMagic, mPeriodicIO.arm_demand);
-//                 break;
-//             default:
-//                 mArm.set(ControlMode.MotionMagic,0.0);
-//                 break;
-//         }
-
-//     }
-
-//     public synchronized void setBrakeMode(boolean brake) {
-//         mArm.setNeutralMode(brake ? NeutralMode.Brake : NeutralMode.Coast);
-//     }
-
-//     public synchronized void resetClimberPosition() {
-//         mArm.setSelectedSensorPosition(0.0);
-//     }
-
-//     public void setArmOpenLoop(double wantedDemand) {
-//         if (mArmControlState != ArmControlState.OPEN_LOOP) {
-//             mArmControlState = ArmControlState.OPEN_LOOP;
-//         }
-//         mPeriodicIO.arm_demand = (wantedDemand > 12 ? 12 : wantedDemand);
-//     }
-
-
-//     public void setArmPosition(double wantedPositionTicks) {
-//         if (mArmControlState != ArmControlState.MOTION_MAGIC) {
-//             mArmControlState = ArmControlState.MOTION_MAGIC;
-//         }
-//         mPeriodicIO.arm_demand = wantedPositionTicks;
-//     }
-
-//     public void setRightClimberPositionDelta(double wantedPositionDelta) {
-//         if (mArmControlState != ArmControlState.MOTION_MAGIC) {
-//             mArmControlState = ArmControlState.MOTION_MAGIC;
-//             mPeriodicIO.arm_demand = mPeriodicIO.arm_motor_position;
-//         }
-//         mPeriodicIO.arm_demand = mPeriodicIO.arm_demand + wantedPositionDelta;
-//     }
-
-
-//     /*** ARM METHODS
-//      * 
-//      *
-//      * */
-
-//     // extend arm
-//     public void setExtendForHybrid() {
-//         setArmPosition(Constants.ArmConstants.kHybridTravelDistance);
-//     }
-
-
-//     // third step for traversal
-//     public void setExtendForMid() {
-//         setArmPosition(Constants.ArmConstants.kMidTravelDistance);
-//     }
-
-
-//     public void setExtendForHigh() {
-//         setArmPosition(Constants.ArmConstants.kHighTravelDistance);
-//     }
-
-//     public void setArmDown() {
-//         setArmPosition(10); // ticks
-//     }
-
-//     // hold current position on arm
-//     //If arm is moving less than 0.5 m/s and arm current is greater than constant stator current limit
-//     public void maybeHoldCurrentPosition() {
-//         if (Util.epsilonEquals(mPeriodicIO.arm_motor_velocity, 0, 0.5)
-//                 && (mPeriodicIO.arm_stator_current > Constants.ArmConstants.kStatorCurrentLimit)) {
-            
-//             setArmPosition(mPeriodicIO.arm_motor_position + 2000);
-//             System.out.println("triggered arm");
-//         }
-//     }
-
-//     public enum ArmControlState {
-//         HOMING,
-//         OPEN_LOOP,
-//         MOTION_MAGIC
-//     }
+    private static Arm mInstance;
+    public static synchronized Arm getInstance() {
+        if (mInstance == null) {
+            mInstance = new Arm();
+        } 
+        return mInstance;
+    }
     
-//     public void stop() {
-//         mIsEnabled = false;
-//         mArm.set(ControlMode.PercentOutput, 0.0);
-//     }
+    TalonFX motor;
 
-//     public double getArmVelocity() {
-//         return mPeriodicIO.arm_motor_velocity;
-//     }
+    public Arm(TalonFX armMotor) {
 
+        motor = armMotor;
 
-//     public double getArmDemand() {
-//         return mPeriodicIO.arm_demand;
-//     }
+    }
 
+    public void resetPosition() {
 
-//     public double getArmPosition() {
-//         return mPeriodicIO.arm_motor_position;
-//     }
+        motor.setSelectedSensorPosition(0);
 
+    }
 
-//     public double getArmCurrent() {
-//         return mPeriodicIO.arm_stator_current;
-//     }
+    public double getPosition() {
 
+        return motor.getSelectedSensorPosition();
+    }
 
-//     public boolean getHomed() {
-//         return mHomed;
-//     }
+    public void maxLength() {
 
-//     public ArmControlState getControlState() {
-//         return mArmControlState;
-//     }
+        while (-1 * (motor.getSelectedSensorPosition()) < 325000) {
+            motor.set(TalonFXControlMode.PercentOutput, -.6);
+        }
 
-//     public boolean checkSystem() {
-//         return true;
-//     }
+        motor.set(TalonFXControlMode.PercentOutput, 0);
 
-//     public void setArmDemand(double demand) {
-//         mPeriodicIO.arm_demand = demand;
-//     }
+    }
 
-//     public void setClimberDemand(double demand) {
-//         mPeriodicIO.arm_demand = demand;
-//     }
+    public void minLength() {
+        while (-1 * (motor.getSelectedSensorPosition()) > 0) {
+            motor.set(TalonFXControlMode.PercentOutput, .6);
+        }
 
-//     public void toggleExtendArm() {
-//         mExtendArm = !mExtendArm;
-//     }
+        motor.set(TalonFXControlMode.PercentOutput, 0);
 
-//     public void togglePartialExtendArm() {
-//         mPartialExtendArm = !mPartialExtendArm;
-//     }
+    }
 
-//     public boolean getExtendArm() {
-//         return mExtendArm;
-//     }
+}
 
-
-//     public boolean getPartialExtendArm() {
-//         return mPartialExtendArm;
-//     }
-
-
-//     public boolean hasEmergency = false;
-
-//     public void outputTelemetry() {
-//         SmartDashboard.putNumber("Climber Demand", mPeriodicIO.arm_demand);
-//         SmartDashboard.putNumber("Climber Voltage ", mPeriodicIO.arm_voltage);
-//         SmartDashboard.putNumber("Climber Current ", mPeriodicIO.arm_stator_current);
-//         SmartDashboard.putBoolean("Extend Arm", getExtendArm());
-//         SmartDashboard.putBoolean("Partial Extend Arm", getPartialExtendArm());
-
-//     }
-
-//     public static class PeriodicIO {
-//         /* Inputs */
-//         public double arm_voltage;
-//         public double arm_stator_current;
-//         public double arm_motor_position;
-//         public double arm_motor_velocity;
-
-//         /* Outputs */
-//         public double arm_demand;
-//     }
-
-//     // logger
-//     @Override
-//     public void registerLogger(LoggingSystem LS) {
-//         SetupLog();
-//         LS.register(mStorage, "ARM_LOGS.csv");
-//     }
-
-//     public void SetupLog() {
-//         mStorage = new LogStorage<PeriodicIO>();
-
-//         ArrayList<String> headers = new ArrayList<String>();
-//         headers.add("timestamp");
-
-//         headers.add("is_enabled");
-        
-//         headers.add("arm_demand");
-
-//         headers.add("arm_motor_position");
-        
-//         headers.add("arm_motor_velocity");
-
-//         headers.add("arm_voltage");
-
-//         headers.add("arm_stator_current");
-
-//         mStorage.setHeaders(headers);
-//     }
-
-//     public void SendLog() {
-//         ArrayList<Number> items = new ArrayList<Number>();
-//         items.add(Timer.getFPGATimestamp());
-
-//         items.add(mIsEnabled ? 1.0 : 0.0);
-
-//         items.add(mPeriodicIO.arm_demand);
-
-//         items.add(mPeriodicIO.arm_motor_position);
-
-//         items.add(mPeriodicIO.arm_motor_velocity);
-
-//         items.add(mPeriodicIO.arm_voltage);
-
-//         items.add(mPeriodicIO.arm_stator_current);
-
-//         // send data to logging storage
-//         mStorage.addData(items);
-//     }
-// } 
